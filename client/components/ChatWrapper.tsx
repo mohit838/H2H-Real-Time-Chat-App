94d1d74e43e9a628732b936e61bf8d6cfaa51f29
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-06-16 13:13:29 +0600

    basic setups of the projects

---
+++ client/components/ChatWrapper.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useState } from "react";
+import Chat from "./Chat";
+import Conversation from "./Conversation";
+import Form from "./Form";
+import Nav from "./components/Nav";
+
+const ChatWrapper = () => {
+  const [sidebar, setSidebar] = useState<boolean>(false);
+  const toggleSidebar = () => {
+    setSidebar(!sidebar);
+  };
+  return (
+    <div className="relative">
+      <Conversation sidebar={sidebar} toggleSidebar={toggleSidebar} />
+      <Nav toggleSidebar={toggleSidebar} />
+      <Chat />
+      <Form />
+    </div>
+  );
+};
+
+export default ChatWrapper;
