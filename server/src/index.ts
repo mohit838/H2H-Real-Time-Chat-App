5bc15497ab389ab2c32c43528351e02578fa61bd
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-06-19 14:13:37 +0600

    Update name and some packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,27 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
 import express from "express";
-import 'dotenv/config'
-import cookieParser from "cookie-parser"
-import cors from "cors"
 import connection from "./config/db";
-import userRoutes from "./routes/user.routes"
+import userRoutes from "./routes/user.routes";
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 // connect database
-connection()
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
-app.use(express.json())
-app.use(cookieParser())
+connection();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
 
 // routes
-app.use('/api', userRoutes);
+app.use("/api", userRoutes);
+
+// Listening port test
 app.listen(PORT, () => {
-    console.log(`You server is running on port number: ${PORT}`)
-})  
+  console.log(`You server is running on port number: ${PORT}`);
+});
