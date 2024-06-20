2d301c74a17b087d304c95c0ab62bb3b79674bf5
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-06-21 00:01:04 +0600

    msg content ui 80 percent done

---
+++ client/src/components/messageContainer/MessageContent.jsx
@@ -0,0 +1,9 @@
+const MessageContent = () => {
+  return (
+    <div className="py-2 flex flex-col h-[42rem] overflow-y-auto overflow-x-hidden">
+      message content
+    </div>
+  );
+};
+
+export default MessageContent;
---
+++ client/src/components/messageContainer/MessageHeader.jsx
@@ -0,0 +1,5 @@
+const MessageHeader = () => {
+  return <div className="h-12 text-xl font-bold">{"TO: Mr. Jane Doe"}</div>;
+};
+
+export default MessageHeader;
