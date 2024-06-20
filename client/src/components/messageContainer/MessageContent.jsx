99046ced98bc7e47bfa1180747adc8475cec6799
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-06-21 00:04:16 +0600

    added main msg design

--- client/src/components/messageContainer/MessageContent.jsx
+++ client/src/components/messageContainer/MessageContent.jsx
@@ -1,7 +1,18 @@
+import Message from "./Message";
+
 const MessageContent = () => {
   return (
     <div className="py-2 flex flex-col h-[42rem] overflow-y-auto overflow-x-hidden">
-      message content
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
     </div>
   );
 };
