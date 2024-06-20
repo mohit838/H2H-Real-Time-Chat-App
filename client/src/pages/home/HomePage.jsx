b75d2a9046ca16f05871483a3ea4c8aa4525fb94
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-06-20 20:37:48 +0600

    make homepage design

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,5 +1,20 @@
+import MessageContainer from "../../components/messageContainer/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="bg-black/90 h-full w-full p-4 flex">
+      <div className="w-1/4 p-4">
+        <Sidebar />
+      </div>
+
+      <div className="divider h-full px-[2px] bg-gray-900 h2h-divider"></div>
+
+      <div className="w-3/4 p-4">
+        <MessageContainer />
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
