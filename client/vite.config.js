1f96a21f26d551732e25705bb85c6c44ff977572
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-09-25 11:32:37 +0600

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3003,
     proxy: {
       "/api": {
         target: "http://localhost:7000",
