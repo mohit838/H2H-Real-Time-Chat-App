711eea2cfe63bf8b5d40acc5d4a187330f5eb5e6
Mohammad Mohitul Islam <mohitul449@gmail.com>
2024-09-26 13:01:34 +0600

    update port for frontend

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3003"],
     methods: ["GET", "POST"],
   },
 });
